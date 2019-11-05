8b7b8510c3014ceecdcec7386d56c04ad592cc5e
juanguzmant <juancamiloguzmant36@gmail.com>
2019-11-04 22:53:51 -0500

    Version semi-final concentrate

--- js/concentrate.js
+++ js/concentrate.js
@@ -3,18 +3,23 @@ var array_card_numbers = [1,1,2,2,3,3,4,4,5,5,6,6,7,7];
 var array_cartas = [];
 var cards_reference = {};
 var cardStack = [];
-
+var array_sections = [];
+var count_to_win;
 function init(){
-
-    for (let i = 0; i <= 13; i++) {
-        array_cartas[i] = document.getElementById('carta_'+i+'');
-        shuffle(array_card_numbers);
-        
+    count_to_win = 0;
+    for (let i = 0; i <=2; i++) {
+        array_sections[i] = document.getElementById(i)
         
     }
+    for (let i = 0; i <= 13; i++) {
+        array_cartas[i] = document.getElementById('carta_'+i+''); 
+    }
+    shuffle(array_card_numbers);
     addCardClass();
 
 }
+
+
 
 
 function addCardClass(){
@@ -68,14 +73,19 @@ function shuffle(array) {
 }
 
 function flip(element){
+   changeClassFlip(element);
+   if(cardStack.length <= 2){
     element.classList.add('flipInY');
     cardStack.push(element);
     reveal(element)
+   }else{
+    //    do nothing, cards remain 
+   }
     
 }
-function unFlip(){
+// function unFlip(){
 
-}
+// }
 
 function check(){
     if(cardStack.length === 2){
@@ -92,44 +102,64 @@ function check(){
                 card2.onclick = null;
                 cards_reference[card1.id.substring(6)][1] = true;
                 cards_reference[card2.id.substring(6)][1] = true;
+                count_to_win = count_to_win + 2;
+                win();
             }
             else{
                 cardStack.pop();
                 cardStack.pop();
                 card1.classList.remove(id1);
                 card2.classList.remove(id2);
-                console.log(cardStack);
+                card1.classList.remove('flipInY');
+                card2.classList.remove('flipInY');
+                
                 
             }
           }, 1000);
+       console.log(cardStack);
        
         
         
     }else{
 
     }
+    
 }
+
 function reveal(card){
     let id = card.id.substring(6);
     card.classList.add(cards_reference[id][0])
     check()
-    win();
+    // win();// Check if win
     
 }
-function win(){// verion machetera
-    let count = 0;
-    for (let i = 0; i < 13; i++) {
-        cards_reference[i][1]==true?count++: count;
-        
-    }
-    if(count === 14){
-    document.getElementById('game_concentrate').classList.add('ocultar');
-    document.getElementById('ganar').classList.remove('ocultar');
+function win(){
+    if(count_to_win === 2){
+        console.log("WIN!!!!!!!!");
+        showSection(1);
     }else{
 
     }
 }
 
+function changeClassFlip(element){
+    if(element.classList.contains('flipInY')){
+        element.classList.remove('flypInY')
+    }
+}
+
+
+function showSection(id){
+    hideSections();
+    array_sections[id].classList.remove("ocultar")
+}
+
+function hideSections(){
+array_sections.forEach(section => {
+    section.classList.add("ocultar")
+    
+});
+}
 
 
 
